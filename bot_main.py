3a79d2ca6c739ec2680140885c939719b5a9f96f
Dmitri228007 <122729384+Dmitri228007@users.noreply.github.com>
2024-06-07 00:43:16 +0400

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -7,6 +7,8 @@ from telegram.ext import CallbackContext, Application, MessageHandler, filters, 
 from tokens import map_static_api_token as static_token
 import maps
 from place_handler import question_info
+from tokens import giga_chat
+from paint import *
 
 MAIN, REVIEW, MARKS, CITY = [1, False], [2, False], [3, False], [4, False, {}]
 LOCAL_DATA = dict()
@@ -81,46 +83,44 @@ async def marks(update: Update, context: CallbackContext):
 
 
 async def choose_city(update: Update, context: CallbackContext):
-    global CITY
-    if not CITY[1]:
-        await update.message.reply_text('Напишите название города который вы хотели бы посетить')
-        CITY[1] = True
-        return CITY[0]
-    elif CITY[1] and not ('selected city' in CITY[2].keys()):
-        user_city = update.message.text
-        CITY[2]['selected city'] = user_city
-        cord = maps.coord(static_token, str(CITY[2]['selected city']))
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={cord[0]},{cord[1]}&spn=0.09,0.09&l=map"
-        await context.bot.send_photo(chat_id=update.message.chat_id, photo=map_request)
-        reply_keyboard = [[KeyboardButton('Посмотреть отели'), KeyboardButton('Посмотреть достопримечательности'),
-                           KeyboardButton('Посмотреть обменники')],
-                          [KeyboardButton('Узнать цены на билеты')],
-                          [KeyboardButton('Другой город'), KeyboardButton('В главное меню')]]
-        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-        await update.message.reply_text('Что будем планировать?', reply_markup=markup)
-        return CITY[0]
-    elif update.message.text == 'Посмотреть отели':
-        await update.message.reply_text(question_info(f'''{CITY[2]['selected city']}Отели'''))
-        return CITY[0]
-
-    elif update.message.text == 'Посмотреть достопримечательности':
-        await update.message.reply_text(question_info(f'''Достопремичательности, {CITY[2]['selected city']}'''))
-        return CITY[0]
-
-    elif update.message.text == 'Посмотреть обменники':
-        await update.message.reply_text(question_info(f'''{CITY[2]['selected city']}, обменники'''))
-        return CITY[0]
-
-    elif update.message.text == 'Узнать цены на билеты':
-        print('Узнать цены на билеты')
-        return CITY[0]
-
-    elif update.message.text == 'Другой город':
-        CITY = [4, False, {}]
-        return CITY[0]
-
-    elif update.message.text == 'В главное меню':
-        return MAIN[0]
+    user_text = update.message.text
+    await update.message.reply_text(giga_chat(user_text))
+    # global CITY
+    # if not CITY[1]:
+    #     await update.message.reply_text('Напишите название города который вы хотели бы посетить')
+    #     CITY[1] = True
+    #     return CITY[0]
+    # elif CITY[1] and not ('selected city' in CITY[2].keys()):
+    #     user_city = update.message.text
+    #     CITY[2]['selected city'] = user_city
+    #     cord = maps.coord(static_token, str(CITY[2]['selected city']))
+    #     map_request = f"http://static-maps.yandex.ru/1.x/?ll={cord[0]},{cord[1]}&spn=0.09,0.09&l=map"
+    #     await context.bot.send_photo(chat_id=update.message.chat_id, photo=map_request)
+    #     reply_keyboard = [[KeyboardButton('Посмотреть отели'), KeyboardButton('Посмотреть достопримечательности')],
+    #                       [KeyboardButton('Узнать цены на билеты')],
+    #                       [KeyboardButton('Другой город'), KeyboardButton('В главное меню')]]
+    #     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    #     await update.message.reply_text('Что будем планировать?', reply_markup=markup)
+    #     return CITY[0]
+    # elif update.message.text == 'Посмотреть отели':
+    #     print(question_info(f'''{CITY[2]['selected city']}Отели'''))
+    #     return CITY[0]
+    #
+    # elif update.message.text == 'Посмотреть достопримечательности':
+    #     print('Посмотреть достопримечательности')
+    #     return CITY[0]
+    #
+    # elif update.message.text == 'Узнать цены на билеты':
+    #     print('Узнать цены на билеты')
+    #     return CITY[0]
+    #
+    # elif update.message.text == 'Другой город':
+    #     CITY = [4, False, {}]
+    #     return CITY[0]
+    #
+    # elif update.message.text == 'В главное меню':
+    #     return MAIN[0]
+    #
 
 
 def main():
