a21dc932d53689126be0135c01198f3f67bb34ed
Dmitri228007 <122729384+Dmitri228007@users.noreply.github.com>
2024-06-07 00:44:26 +0400

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,5 +1,1 @@
-tg_bot_token = '6717100798:AAFwFyXJ5yBzDVt3WOISlbvqWx__51iNSpo'
-# map_static_api_token = '4c649912-0fba-4c90-8199-4bb44cb6940a'
-map_static_api_token = '40d1649f-0493-4b70-98ba-98533de7710b'
-map_search_api_token = '633870d0-426c-43c5-96ee-be30e550827e'
-buy_api_token = 'b33f31a2-4879-42c3-b125-2338cdd0c397'
+
